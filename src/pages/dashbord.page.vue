1dcddf5e5ecfaf4b6fc7250a100b33625bc778e2
Emmanuel Diomande <emmanuel.diomande@outlook.com>
2024-07-10 10:49:45 +0000

    Version App 0.6.56 Tms

--- src/pages/dashbord.page.vue
+++ src/pages/dashbord.page.vue
@@ -1,7 +1,7 @@
 <template>
   <BaseLayout>
     <template v-slot:content>
-      <div class="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-2 w-full">
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-2 w-full" v-if="useModalStore().MenuSideBar" >
         <div
           v-for="DT_Menu in useModalStore().MenuSideBar"
         >
