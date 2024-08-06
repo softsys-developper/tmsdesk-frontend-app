478e980b4b8bd9c8b597ef43e6cd5ca2a2e711ce
Emmanuel Diomande <emmanuel.diomande@outlook.com>
2024-08-06 17:32:08 +0000

    Version App 0.6.75 Softsys

--- src/services/app.services.ts
+++ src/services/app.services.ts
@@ -1,20 +1,25 @@
-const Usimeca = {
-    name: 'Usimeca',
-    url: 'erp.usimeca.ci',
-    api: 'https://api.usimeca.ci/api',
-    logo: ''
-}
+// const Usimeca = {
+//   name: "Usimeca",
+//   url: "erp.usimeca.ci",
+//   api: "https://api.usimeca.ci/api",
+//   logo: "",
+// };
 
-const Tmsdesk = {
-    name: 'TRANSLOG MULTI-SERVICES',
-    url: 'tmsdesk.com',
-    api: 'https://apps.tmsdesk.com/api',
-    logo: ''
-}
+const Softsys = {
+  name: "Softsys",
+  url: "gestim.softsys.ci",
+  api: "https://gestim.softsys.ci/api",
+  logo: "",
+};
 
+// const Tmsdesk = {
+//     name: 'TRANSLOG MULTI-SERVICES',
+//     url: 'tmsdesk.com',
+//     api: 'https://apps.tmsdesk.com/api',
+//     logo: ''
+// }
 
-export const isApp:any = 'usimecaKey'
+export const isApp: any = "Softsys";
 export const AppService = () => {
-    return isApp == 'usimeca' ? Usimeca : Tmsdesk
-}
-
+  return Softsys;
+};
